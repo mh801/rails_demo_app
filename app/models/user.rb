930b40c53e4d75b709cebca1bb99925a98c720bc
mh801 <matt.l.hardy@gmail.com>
2012-08-14 14:32:00 -0600

    micropost controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :city, :email, :first_name, :last_name, :state, :zip_code
+  validates_presence_of :email,:first_name,:last_name,:city,:state,:zip_code
+  validates_uniqueness_of :email
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 DemoApp::Application.routes.draw do
-  resources :users
+  resources :microposts
 
+  resources :users
+  root :to => 'users#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
